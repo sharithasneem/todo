9ca50fb91b70d1331c0cf03d50fbf426af2514a5
- <sharibathasneem@gmail.com>
2022-12-19 23:33:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[todos,settodos]=useState([])
+  const[todo,settodo]=useState(" ")
+  return (  
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🙌☕ </h2>
+      </div>
+      <div className="input">
+        <input value={todo} onChange={(e)=>settodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>settodos([...todos,{id:Date.now(),text:todo,Status:false}])}className="fas fa-plus"></i>
+      </div>
+      
+      <div className="todos">
+        {todos.map((value)=> {
+           return (<div className="todo">
+            <div className="left">
+              <input onChange={(e)=>{ 
+                settodos(todos.filter(obj2=>{
+                  if(obj2.id===value.id){
+                    obj2.status=e.target.checked
+                  }
+                  return obj2
+                }))}}  type="checkbox" name="" id="" />
+              <p>{value.text}</p>
+            </div>
+            <div className="right">
+            <i onClick={(e)=>{
+                todos.splice(0, 1);
+                settodos([...todos]);
+            }}>-</i>
+              
+            </div>
+          </div>)
+        })}
+        
+      </div>
+      {todos.map((value)=>{
+            if(value.status){
+              return(<div>
+                <h3><span style={{fontSize:'15 px'}}>Completed Task :</span>{value.text}</h3>
+                 </div>)
+            }
+            return null
+        })}
     </div>
-  );
+  )
+
 }
 
 export default App;
